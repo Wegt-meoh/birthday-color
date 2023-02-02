b6900af91b6110240f003178c9ef507c53af7e71
lasting <lastingcoder@qq.com>
2023-02-02 17:35:06 +0800

    fade in and fade out

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,40 @@
-import { useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import "./App.css";
 import { ColorBoard } from "./components/ColorBoard";
 import { data } from "./data";
 
 function App() {
-  return (
-    <div className="App">
-      {data.map((item, index) => {
-        const {
-          bgColor,
-          textColor,
-          text1,
-          text2,
-          dateText: title1,
-          colorName: title2,
-        } = item;
-        return (
-          <div key={index}>
+  const [datas, setDatas] = useState<JSX.Element[]>([]);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      const newDatas = [];
+      let newIndex = index;
+      for (let i = 0; i < 4; i += 1) {
+        if (newIndex >= data.length) {
+          newIndex = 0;
+        }
+        newDatas.push(
+          <div key={newIndex}>
             <ColorBoard
-              bgColor={bgColor}
-              textColor={textColor}
-              dateText={title1}
-              colorName={title2}
-              text1={text1}
-              text2={text2}
+              bgColor={data[newIndex].bgColor}
+              textColor={data[newIndex].textColor}
+              dateText={data[newIndex].dateText}
+              colorName={data[newIndex].colorName}
+              text1={data[newIndex].text1}
+              text2={data[newIndex].text2}
             />
           </div>
         );
-      })}
-    </div>
-  );
+        newIndex += 1;
+      }
+      setIndex(newIndex);
+      setDatas(newDatas);
+    }, 9000);
+  }, [index]);
+
+  return <div className="App">{datas}</div>;
 }
 
 export default App;
